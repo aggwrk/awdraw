83b44b76e1e9cf10f4d48930c7fb969358af1a52
Agung Wirakurniawan <45479562+aggwrk@users.noreply.github.com>
2025-05-21 12:25:43 +0700

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -13,8 +13,7 @@ export function Badge({
       target="_blank"
       {...props}
     >
-      <picture>
-        <Button
+      <picture
         className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
         asChild
       >
@@ -22,7 +21,7 @@ export function Badge({
           <VercelIcon size={16} />
           @agung_wirak
         </a>
-      </Button>
+      
       </picture>
     </a>
   );
