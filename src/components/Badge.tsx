148412f7c4050df62c1aac8b2ad89521ab68d283
Agung Wirakurniawan <45479562+aggwrk@users.noreply.github.com>
2025-05-21 12:24:05 +0700

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -14,15 +14,15 @@ export function Badge({
       {...props}
     >
       <picture>
-        <source
-          srcSet="https://liveblocks.io/badge-dark.svg"
-          media="(prefers-color-scheme: dark)"
-        />
-        <img
-          src="https://liveblocks.io/badge-light.svg"
-          alt="Made with Liveblocks"
-          className={styles.image}
-        />
+        <Button
+        className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
+        asChild
+      >
+        <a href={`https://www.instagram.com/agung_wirak`} target="_noblank" rel="noopener noreferrer">
+          <VercelIcon size={16} />
+          @agung_wirak
+        </a>
+      </Button>
       </picture>
     </a>
   );
